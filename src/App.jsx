79ca6e753760436e4a0c1f6f5ee004ad9db0c051
Livia <liviamtavares05@gmail.com>
2025-09-10 20:33:24 -0300

    feat: Contador de Cliques

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import CatApi from './pages/Cat Api'
 import Home from './pages/Home'
+import Contador from './pages/Contador'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catApi" element={<CatApi />} />
+        <Route path="/count" element={<Contador />} />
       </Routes>
     </>
   )
